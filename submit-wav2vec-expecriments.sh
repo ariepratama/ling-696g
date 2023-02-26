6e88010d3e05be23403068647a2ba7caf38b2ab8
Arie Pratama Sutiono <arie.pratama@ninjavan.co>
2023-02-26 21:43:35 +0800

    change output neuron to be the same as dnn neuron

--- submit-wav2vec-expecriments.sh
+++ submit-wav2vec-expecriments.sh
@@ -30,7 +30,7 @@ sbatch \
 
 
 sbatch \
-  --job-name=wav2vec-arie \
+  --job-name=wav2vec-arie-521 \
   --time="10:00:00" \
   --partition=standard \
   --account=clu-ling \
@@ -41,4 +41,33 @@ sbatch \
   --cpus-per-task=1 \
   --gres=gpu:1 \
   --mem=8GB \
-  train_id_wav2vec_experiments.sh hparams/stt_experiments/train_id_with_wav2vec_42.yaml
+  train_id_wav2vec_experiments.sh hparams/stt_experiments/train_id_with_wav2vec_521.yaml
+
+  sbatch \
+  --job-name=wav2vec-arie-522 \
+  --time="10:00:00" \
+  --partition=standard \
+  --account=clu-ling \
+  --output=%x-%j.out \
+  --mail-type=ALL \
+  --mail-user=ariesutiono@arizona.edu \
+  --ntasks=1 \
+  --cpus-per-task=1 \
+  --gres=gpu:1 \
+  --mem=8GB \
+  train_id_wav2vec_experiments.sh hparams/stt_experiments/train_id_with_wav2vec_522.yaml
+
+
+sbatch \
+  --job-name=wav2vec-arie-523 \
+  --time="10:00:00" \
+  --partition=standard \
+  --account=clu-ling \
+  --output=%x-%j.out \
+  --mail-type=ALL \
+  --mail-user=ariesutiono@arizona.edu \
+  --ntasks=1 \
+  --cpus-per-task=1 \
+  --gres=gpu:1 \
+  --mem=8GB \
+  train_id_wav2vec_experiments.sh hparams/stt_experiments/train_id_with_wav2vec_523.yaml
